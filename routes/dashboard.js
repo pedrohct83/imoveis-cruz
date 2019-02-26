adf071fd5e7555c7cb9a9e1b580e8f350bf6703b
Pedro Cruz <pedrohct83@gmail.com>
2019-02-26 00:00:23 +0000

    installed optimist and created a realty updater script, started the dashboard page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,26 @@
 var express = require("express"),
     router = express.Router(),
+    Realty = require("../models/realty"),
     middleware = require("../middleware/");
 
 // INDEX - Show dashboard page
 router.get("/", middleware.isLoggedIn, middleware.isAdmin, function(req, res) {
-    res.render("dashboard/index");
+    Realty.find().exec(function(err, allRealty) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            Realty.find({ isRented: "Sim" }).exec(function(err, occupiedRealty) {
+                if (err) {
+                    console.log(err);
+                    res.redirect("back");
+                }
+                else {
+                    res.render("dashboard/index", { allRealty, occupiedRealty });
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
--- scripts/realty-updater.js
+++ scripts/realty-updater.js
@@ -1,12 +1,43 @@
-var Realty = require("../models/realty");
-function test() {
-    console.log("in test function");
-    Realty.find().exec(function(err, realty) {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("realty found");
-        }
-    });
+var Realty = require("../models/realty"),
+    mongoose = require("mongoose");
+
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/imoveis";
+mongoose.connect(url, { useNewUrlParser: true });
+
+var myArgs = require('optimist').argv,
+    help = 'Realty Updater 1.0\nRode o script com as flags: -n realty_name -k realty_key -v realty_key_newvalue';
+
+if ((myArgs.h) || (myArgs.help)) {
+    console.log(help);
+    process.exit(0);
 }
-test();
+
+if (myArgs.n && myArgs.k && myArgs.v) {
+    console.log(`All flags detected: ${myArgs.n} ${myArgs.k} ${myArgs.v}`);
+    var realtyName = new RegExp(escapeRegex(myArgs.n), 'gi');
+    (async () => {
+        let realtyName = new RegExp(escapeRegex(myArgs.n), 'gi');
+        let promise = await Realty.find({ name: realtyName });
+        if (promise.length === 0) {
+            console.log("No realty found with that name");
+        } else {
+            try {
+                console.log(`${promise.length} realty found with the name ${myArgs.n}`);
+                promise.forEach(function(realty) {
+                   realty[myArgs.k] = myArgs.v;
+                   realty.save();
+                });
+                console.log(`Set the key "${myArgs.k}" with the value ${myArgs.v} for ${promise.length} realty found with the name "${myArgs.n}"`);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    })();
+} else {
+    console.log(help);
+    process.exit(0);
+}
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
