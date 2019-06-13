4d1a135ab6b65bcc1f852870a901c261dcaea85c
Pedro Cruz <pedrohct83@gmail.com>
2019-06-13 21:02:49 +0000

    all models are now commented

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,8 @@ var mongoose = require("mongoose");
 var commentSchema = new mongoose.Schema({
     text: String,
     createdAt: { type: Date, default: Date.now },
+    // Comment has only one author, which is a user
+    // We decided to store only id and username in this comment schema
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
--- models/realty.js
+++ models/realty.js
@@ -1,7 +1,8 @@
 var mongoose = require("mongoose");
 
 var realtySchema = new mongoose.Schema({
-    isFamily: Boolean,
+    // A realty has only one author, which is a user
+    // We decided to store only id and username in this realty schema
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
@@ -9,6 +10,7 @@ var realtySchema = new mongoose.Schema({
         },
         username: String
     },
+    // In order to perform a text search on the content of this field, we had to index it with a text index (text:true)
     name: { type: String, text: true },
     type: String,
     owner: String,
@@ -17,8 +19,19 @@ var realtySchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now },
     areaSize: { type: Number, min: 1 },
     fiscalNum: { type: Number, min: 1 },
+    // Get and Set calls getPrice and setPrice functions
+    // We are storing currency numbers like 123.45 as 1245 (set), but when we get it, we need it as 123.45 (get)
     condominiumValue: {type: Number, get: getPrice, set: setPrice},
     iptuValue: {type: Number, get: getPrice, set: setPrice},
+    notes: String,
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ],
+    
+    // Tenant related fields
     isRented: String,
     tenant: {
         id: {
@@ -28,18 +41,13 @@ var realtySchema = new mongoose.Schema({
         name: String,
         notes: String
     },
+    isFamily: Boolean,
     contractStart: Date,
     contractEnd: Date,
-    rentValue: {type: Number, get: getPrice, set: setPrice},
-    notes: String,
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment"
-        }
-    ],
+    rentValue: {type: Number, get: getPrice, set: setPrice}
 });
 
+// toFixed(2) converts a number into a string, keeping only two decimals
 function getPrice(num){
     return (num/100).toFixed(2);
 }
